7ecdd6c96c4358bcf2f27c9b269048b846fd9cfe
Pete McLaughlin <Petemcl1978@gmail.com>
2019-05-08 20:32:45 -0400

    and so it begins. trouble shooting

--- train.js
+++ train.js
@@ -1,7 +1,94 @@
-$(document).ready(function() {
-    //initialize firebase
 
-    var config ={
-        apiKey: 
-    }
-}
+$( document ).ready(function(){
+var firebaseConfig = {
+    apiKey: "AIzaSyB08iW_fkBw_GkdG90HedE26GhI7VNe_g8",
+    authDomain: "trainschedule-21cc6.firebaseapp.com",
+    databaseURL: "https://trainschedule-21cc6.firebaseio.com",
+    projectId: "trainschedule-21cc6",
+    storageBucket: "trainschedule-21cc6.appspot.com",
+    messagingSenderId: "713989575518",
+    appId: "1:713989575518:web:bbc3bcf5ab0ccae8"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+    var database= firebase.dataBase();
+
+//button to submit the info from user
+$("#trainInfobtn").on("click", function(event){
+    event.preventDefault();//no button reset
+
+    //set variables of user input
+    var trainName =
+    $("#name").val().trim();
+    var destination = $("#dest").val().trim();
+
+    //converts user input to usable information
+
+    var firstTime = moment($("#firstTime").val().trim(),
+    "hh:mm").subract(1,"years").format("X");
+
+    var frequency = $("#freq").val().trim();
+
+    //current time
+    var currentTime = moment();
+    console.long("Current Time: " + moment(currentTime).format("hh:mm"));
+
+        //brings all new train info
+
+        var newTrain = {
+            train: trainName,
+            trainGoing: destination,
+            trainComing: firstTime,
+            everyXMin: frequency
+        };
+
+        //uploading newTrain to firebase
+        database.ref().push(newTrain);
+        //*push* adds info in database.*set* overwrites existing
+        MediaDeviceInfo
+        
+        //clear elements 
+
+        $("#name").val("");
+        $("#dest").val("");
+        $("firstTime").val("");
+        $("#freq").val("");
+
+
+        return false;
+    });
+
+    database.ref().on("child_added", function(childSnapshot,preChildKey){
+        console.log(childSnapshot.val());
+
+        var trainName =
+        childSnapshot.val().train;
+        var destination =
+        childSnapshot.cal().trainGoing;
+        var firstTime =
+        childSnapshot.val().trainComing;
+        var frequency = childSnapshot.val().everyXMin;
+
+
+        //tidy up first train time 
+        var trainTime = moment.unix(firstTime).format("hh:mm");
+        //math to figure out time diff between trains
+        var difference= moment().diff(moment(trainTime),"minutes");
+
+        //time apart
+        var trainRemain = difference % frequency;
+        //min till arrive
+        var minUntill = frequency - trainRemain;
+
+        //NEXT
+
+        var nextArrival = moment().add(minUntil,"minutes").format('hh:mm');
+
+        //adding info to Dom Table
+
+        $("#trainTable > tbody").append("<tr><td>" + trainName + "</td><td>" + destination + "</td><td>" + nextArrival + "</td><td>" +minUntil + "</td><tr>");
+
+    });
+});
+
